f3ae0e37f9e3a191e5df612b8ca8f68556d31a90
spyke2809 <spyke2809.tobias@gmail.com>
2025-04-21 16:02:40 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+// Dummy Functions for Button Interaction
+document.querySelector('.like-btn').addEventListener('click', () => {
+    alert('Gefällt mir geklickt!');
+});
+
+document.querySelector('.comment-btn').addEventListener('click', () => {
+    alert('Kommentar hinzugefügt!');
+});
